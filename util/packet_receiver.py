10c532e002523d33cc270ee9938fce5462312fe6
JiaJing <jiajingbupt@163.com>
2023-06-22 20:53:15 +0800

    AH packet constructing.

--- util/packet_receiver.py
+++ util/packet_receiver.py
@@ -5,7 +5,7 @@ class UdpReceiver(object):
         self.interface = interface
 
     def udp_sniff(self):
-        filter = '198.2.6.9'
+        filter = 'net fe80::dead:beef'
         store = 1
         count = 0
         # packet = sniff(filter= filter, store = True, iface = self.interface,prn=lambda x:x.summary())
@@ -14,7 +14,73 @@ class UdpReceiver(object):
         print(packet)
 
     def packet_show(self, packet):
-        packet.show()
+        layers = packet.layers()
+        self.print_layers(layers)
+        # print("--- parse ip ----")
+        # self.parse_ip(packet)
+        # print("--- parse ether ---")
+        # self.parse_ether(packet)
+        print("--- parse ipv6 ---")
+        self.parse_ipv6(packet)
+
+        print("--- parse AH ---")
+        self.parse_AH(packet)
+        # print("--- parse udp ---")
+        # self.parse_udp(packet)
+
+        # packet.show(dump = False)
+
+    def parse_ip(self,packet):
+        ip = packet['IP']
+        print("--- src ---")
+        src = ip.src
+        print(src)
+        print('--- dst ---')
+        dst = ip.dst
+        print(dst)
+        ip.show()
+    
+    def parse_ether(self, packet):
+        ether = packet['Ethernet']
+        print(ether)
+        ether.show()
+    
+    def parse_udp(self, packet):
+        udp = packet['UDP']
+        print(udp)
+        udp.show()
+
+    def parse_ipv6(self, packet):
+        ipv6 = packet['IPv6']
+        print(ipv6)
+        print("--- src ---")
+        src = ipv6.src
+        print(src)
+        print("--- dst ---")
+        dst = ipv6.dst
+        print(dst)
+
+        ipv6.show()
+
+    def parse_AH(self, packet):
+        ah = packet['AH']
+        print(ah)
+        print("--- spi ---")
+        # spi --> Identifier of PAH，32bit 
+        spi = ah.spi
+        print(spi)
+        print("--- seq ---")
+        # seq --> OTP（One-time password）value，32bit
+        seq = ah.seq
+        print(seq)
+
+        
+
+
+    def print_layers(self, layers):
+        for i in range(len(layers)):
+            print("--- layer %s---", layers[i].__name__)
+            print(layers[i])
 
 
 if __name__ == '__main__':
--- util/packet_sender.py
+++ util/packet_sender.py
@@ -5,11 +5,41 @@ class Packet_Generator(object):
         pass
 
     def send(self, interface, dst_ip):
-        packet = Ether(src = '00:00:00:00:00:01', dst = '00:00:00:00:00:02')/IP(dst = dst_ip)/UDP(dport = 53)
+        packet = Ether(src = '00:00:00:00:00:01', dst = '00:00:00:00:00:02')/IPv6(dst = dst_ip)/UDP(dport = 53)
         print(packet)
         sendp(packet,iface=interface)
+
+    def send_extension(self, interface):
+        base = IPv6()
+        base.src = 'fe80::dead:beef'
+        base.dst = 'fe80::1234'
+        extension = IPv6ExtHdrHopByHop()
+        jumbo = Jumbo()
+        jumbo.jumboplen = 2**30
+        extension.options = jumbo
+        packet = Ether(src = '00:00:00:00:00:01', dst = '00:00:00:00:00:02')/base/extension/UDP(dport = 4953)
+        packet.show()
+        sendp(packet, iface= interface)
+
+    def send_extension_AH(self, interface):
+        base = IPv6()
+        base.src = 'fe80::dead:beef'
+        base.dst = 'fe80::1234'
+
+        extension = AH()
+        extension.payloadlen = 2     # 8bit
+        extension.nh = 17            # 8bit 
+        extension.spi = 123456       # 32bit
+        extension.seq = 2341833143   # 32bit
+        extension.icv = 1111        # 128bit
+        extension.padding = 0
+        packet = Ether(src = '00:00:00:00:00:01', dst = '00:00:00:00:00:02')/base/extension/UDP(dport = 4953)
+        packet.show()
+        sendp(packet, iface= interface)
+
 
 
 if __name__ == '__main__':
     packet_sender = Packet_Generator()
-    packet_sender.send(interface= 'ens33', dst_ip= '198.2.6.9')
+    # packet_sender.send(interface= 'ens33', dst_ip= '2001:db8:cafe:f000::')
+    packet_sender.send_extension_AH(interface= 'ens33')
